b0c039efdd0b6e91b75daac3191c0308a6e15cad
Maham-Javed <mahamjavedb35@gmail.com>
2024-10-03 21:54:56 +0500

    make file structure and scheme with pre middleware.

---
+++ modals/user.js
@@ -0,0 +1,54 @@
+const { createHmac, randomBytes } = require("crypto");
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      require: true,
+    },
+    passward: {
+      type: String,
+      require: true,
+    },
+    profileImageURL: {
+      type: String,
+      default: "/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", ADMIN],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("passward")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassward = createHmac("sha256", salt)
+    .update(user.passward)
+    .digest("hex");
+
+  this.salt = salt;
+  this.passward = hashedPassward;
+
+  next();
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
