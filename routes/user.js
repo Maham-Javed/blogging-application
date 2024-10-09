b478b88e9b70e39a74660d12e4059ea1a119da7c
Maham-Javed <mahamjavedb35@gmail.com>
2024-10-09 18:47:36 +0500

    update: implement authentication in my blogging application by using json web token(JWT) in services module.

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const User = require("../modals/user");
+const { JsonWebTokenError } = require("jsonwebtoken");
 
 const router = Router();
 
@@ -15,10 +16,14 @@ router.get("/signup", (req, res) => {
 router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.matchPassword(email, password);
-
-  console.log("User", user);
-  return res.redirect("/");
+  try {
+    const token = await User.matchPasswordAndCreateToken(email, password);
+    return res.cookie("token", token).redirect("/");
+  } catch (error) {
+    return res.render("signin", {
+      error: "Incorrect Email or Password",
+    });
+  }
 });
 
 // Sign-up post route.
